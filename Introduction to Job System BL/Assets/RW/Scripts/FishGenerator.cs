f8f818bd5d7111cac40bea8d183e5f26f21bf812
Brian Leip <brianleip@gmail.com>
2021-08-19 17:11:00 -0700

    Completed Intro to Job System tutorial yesterday. Committing today

---
+++ Introduction to Job System BL/Assets/RW/Scripts/FishGenerator.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Jobs;
+using Unity.Collections;    // for NativeArrays
+using Unity.Burst;
+using UnityEngine.Jobs;
+using math = Unity.Mathematics.math;
+using random = Unity.Mathematics.Random;
+
+public class FishGenerator : MonoBehaviour
+{
+    [Header("References")]
+    public Transform waterObject;
+    public Transform fishPrefab;
+    public Transform fishParent;
+    // velocities keep track of the velocity of each fish throughout the lifetime of the game, so that you can simulate continuous movement.
+    private NativeArray<Vector3> velocities;
+    // TransformAccessArray is used in place of a NativeArray of transforms, as you can’t pass reference types between threads. So, Unity provides a TransformAccessArray, which contains the value type information of a transform including its position, rotation and matrices. The added advantage is, any modification you make to an element of the TransformAccessArray will directly impact the transform in the scene.
+    private TransformAccessArray transformAccessArray;
+
+    [Header("Spawn Settings")]
+    public int amountOfFish;
+    public Vector3 spawnBounds;
+    public float spawnHeight;
+    public int swimChangeFrequency;
+    private PositionUpdateJob positionUpdateJob;
+    private JobHandle positionUpdateJobHandle;
+
+    [Header("Settings")]
+    public float swimSpeed;
+    public float turnSpeed;
+
+    private void Start() {
+        // Initialize velocities with a persistent allocator of size amountOfFish, which is a pre-declared variable.
+        velocities = new NativeArray<Vector3>(amountOfFish, Allocator.Persistent);
+        // Initialize transformAccessArray with size amountOfFish.
+        transformAccessArray = new TransformAccessArray(amountOfFish);
+
+        // Instantiate the fish gameObjects and populate the transform access array with the fish transforms
+        for (int i=0; i<amountOfFish; i++) {
+            float distanceX = Random.Range(-spawnBounds.x / 2, spawnBounds.x / 2);
+            float distanceZ = Random.Range(-spawnBounds.z / 2, spawnBounds.z / 2);
+            // Create a random spawn point within spawnBounds.
+            Vector3 spawnPoint = (transform.position + Vector3.up * spawnHeight) + new Vector3(distanceX, 0, distanceZ);
+            // Instantiate objectPrefab, which is a fish, at spawnPoint with no rotation.
+            Transform t = (Transform)Instantiate(fishPrefab, spawnPoint, Random.rotation, fishParent);
+            // Add the instantiated transform to transformAccessArray.
+            transformAccessArray.Add(t);
+        }
+    }
+
+    private void Update() {
+        // all the variables within the main thread set the job's data. seed gets the current millisecond from the system time to ensure a different seed for each call.
+        positionUpdateJob = new PositionUpdateJob() {
+            objectVelocities = velocities,
+            jobDeltaTime = Time.deltaTime,
+            swimSpeed = this.swimSpeed,
+            turnSpeed = this.turnSpeed,
+            time = Time.time,
+            swimChangeFrequency = this.swimChangeFrequency,
+            center = waterObject.position,
+            bounds = spawnBounds,
+            seed = System.DateTimeOffset.Now.Millisecond
+        };
+
+        // schedule positionUpdateJob. Note that each job type has its own Schedule() parameters. A IJobParallelForTransform takes a TransformAccessArray.
+        positionUpdateJobHandle = positionUpdateJob.Schedule(transformAccessArray);
+    }
+
+    private void LateUpdate() {
+        positionUpdateJobHandle.Complete();
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireCube(transform.position + Vector3.up * spawnHeight, spawnBounds);
+    }
+
+    private void OnDestroy(){ 
+        transformAccessArray.Dispose();
+        velocities.Dispose();
+    }
+
+    [BurstCompile] struct PositionUpdateJob : IJobParallelForTransform {
+        public NativeArray<Vector3> objectVelocities;
+
+        public Vector3 bounds;
+        public Vector3 center;
+
+        public float jobDeltaTime;
+        public float time;
+        public float swimSpeed;
+        public float turnSpeed;
+        public int swimChangeFrequency;
+
+        public float seed;
+
+        // NOTE - this version of Execute also passes a TransformAccess variable
+        public void Execute(int i, TransformAccess transform) {
+            // Sets the current velocity of the fish.
+            Vector3 currentVelocity = objectVelocities[i];
+            // Uses Unity's Mathematics library to create a psuedorandom number generator that creates a seed by using the index and system time.
+            random randomGen = new random((uint)(i * time + 1 + seed));
+            // Moves the transform along its local forward direction, using localToWorldMatrix.
+            transform.position += transform.localToWorldMatrix.MultiplyVector(new Vector3(0,0,1)) * swimSpeed * jobDeltaTime * randomGen.NextFloat(0.3f, 1.0f);
+
+            // Rotates the transform in the direction of currentVelocity.
+            if (currentVelocity != Vector3.zero) {
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(currentVelocity), turnSpeed * jobDeltaTime);
+            }
+
+            // Prevent fish out of water
+            Vector3 currentPosition = transform.position;
+
+            bool randomize = true;
+
+            // check the position of the transform against the boundaries. If it's outside, the velocity flips towards the center.
+            if (currentPosition.x > center.x + bounds.x / 2 ||
+                currentPosition.x < center.x - bounds.x / 2 ||
+                currentPosition.z > center.z + bounds.z / 2 ||
+                currentPosition.z < center.z - bounds.z / 2 )
+            {
+                Vector3 internalPosition = new Vector3(
+                    center.x + randomGen.NextFloat(-bounds.x / 2, bounds.x / 2)/1.3f, 
+                    0, 
+                    center.z + randomGen.NextFloat(-bounds.z / 2, bounds.z / 2)/1.3f);
+
+                currentVelocity = (internalPosition - currentPosition).normalized;
+
+                objectVelocities[i] = currentVelocity;
+
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(currentVelocity), turnSpeed * jobDeltaTime * 2);
+
+                randomize = false;
+            }
+
+            // If the transform is within the boundaries, there's a small possibility that the direction will shift to give the fish a more natural movement.
+            if (randomize) {
+                if (randomGen.NextInt(0, swimChangeFrequency) <= 2) {
+                    objectVelocities[i] = new Vector3(randomGen.NextFloat(-1f, 1f), 0, randomGen.NextFloat(-1f, 1f));
+                }
+            }
+
+        }
+    }
+}
---
+++ Introduction to Job System BL/Assets/RW/Scripts/WaveGenerator.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Jobs;
+using Unity.Jobs;
+using Unity.Collections;    // optimized version of System.Collections built for safe multi-threading. Includes NativeArray, NativeList, Native Queue, NativeHashMap, NativeMultiHashMap.
+using Unity.Burst;
+using Unity.Mathematics;
+
+public class WaveGenerator : MonoBehaviour {
+    [Header("Wave Parameters")]
+    public float waveScale;
+    public float waveOffsetSpeed;
+    public float waveHeight;
+    NativeArray<Vector3> waterVertices;
+    NativeArray<Vector3> waterNormals;
+    /* This JobHandle serves three primary functions:
+        - Scheduling a job correctly.  “Schedule Early, Complete Late”
+        - Making the main thread wait for a job’s completion.
+        - Adding dependencies. Dependencies ensure that a job only starts after another job completes. This prevents two jobs from changing the same data at the same time. It segments the logical flow of your game. */
+    JobHandle meshModificationJobHandle;
+    // Reference an UpdateMeshJob (custom struct below) so the entire class can access it.
+    UpdateMeshJob meshModificationJob;
+
+    [Header("References and Prefabs")]
+    public MeshFilter waterMeshFilter;
+    private Mesh waterMesh;
+
+
+    private void Start() {
+        waterMesh = waterMeshFilter.mesh;
+        // mark the waterMesh as dynamic so Unity can optimize sending vertex changes from the CPU to the GPU.
+        waterMesh.MarkDynamic();
+
+        // Initialize waterVertices and waterNormals with the vertices/normals of the waterMesh. Also assign a persistent allocator.
+        waterVertices = new NativeArray<Vector3>(waterMesh.vertices, Allocator.Persistent);
+        waterNormals = new NativeArray<Vector3>(waterMesh.normals, Allocator.Persistent);
+        /* ALLOCATOR TYPES:
+            - Temp: Designed for allocations with a lifespan of one frame or less, it has the fastest allocation. It’s not allowed for use in the Job System.
+            - TempJob: Intended for allocations with a lifespan of four frames, it offers slower allocation than Temp. Small jobs use them.
+            - Persistent: Offers the slowest allocation, but it can last for the entire lifetime of a program. Longer jobs can use this allocation type. (also when you set the NativeArray once and it stays the same without needing to re-initialize after job finishes.)
+        */
+    }
+
+    private void Update() {
+        // "Schedule early (update), complete late (Late update)"
+        // initialize the UpdateMeshJob with all the variables required for the job.
+        meshModificationJob = new UpdateMeshJob() {
+            vertices = waterVertices,
+            normals = waterNormals,
+            offsetSpeed = waveOffsetSpeed,
+            time = Time.time,
+            scale = waveScale,
+            height = waveHeight
+        };
+        // The IJobParallelFor’s Schedule() requires the length of the loop and the batch size. The batch size determines how many segments to divide the work into.  Once scheduled, you cannot interrupt a job.
+        meshModificationJobHandle = meshModificationJob.Schedule(waterVertices.Length, 64);
+    }
+
+    private void LateUpdate() {
+        // "Schedule early (update), complete late (Late update)"
+        
+        // Ensures the completion of the job because you can’t get the result of the vertices inside the job before it completes.
+        meshModificationJobHandle.Complete();   
+        // Unity allows you to directly set the vertices of a mesh from a job. This is a new improvement that eliminates copying the data back and forth between threads.
+        waterMesh.SetVertices(meshModificationJob.vertices);
+        // recalculate the normals of the mesh so that the lighting interacts with the deformed mesh correctly.
+        waterMesh.RecalculateNormals();
+    }
+
+    [BurstCompile]
+    private struct UpdateMeshJob : IJobParallelFor {
+        /* JOB TYPES (INTERFACES)
+            - IJob: The standard job, which can run in parallel with all the other jobs you’ve scheduled. Used for multiple unrelated operations.
+            - IJobParallelFor: All ParallelFor jobs allow you to perform the same independent operation for each element of a native container within a fixed number of iterations. Unity will automatically segment the work into chunks of defined sizes.
+            - IJobParallelForTransform: A ParallelFor job type that’s specialized to operate on transforms.
+            --- NOTE: each job must be a struct, and must have an Execute() method
+        */
+        public NativeArray<Vector3> vertices;   // a public NativeArray to read and write vertex data between the job and the main thread.
+        
+        [ReadOnly] // The [ReadOnly] tag tells the Job System that you only want to read the data from the main thread.
+        public NativeArray<Vector3> normals; 
+
+        // These variables control how the Perlin noise function acts. The main thread passes them in.
+        public float offsetSpeed;
+        public float scale;
+        public float height;
+        // Note that you cannot access statics such as Time.time within a job. Instead, you pass them in as variables during the job’s initialization.
+        public float time;
+
+        private float Noise(float x, float y) {
+            float2 pos = math.float2(x, y);
+            return noise.snoise(pos);   // note - snoise is actually simplex noise not perlin (similar but improved and not copyrighted)
+        }
+
+        public void Execute(int i) {
+            // ensure the wave only affects the vertices facing upwards. This excludes the base of the water.
+            if (normals[i].z > 0f) {
+                var vertex = vertices[i];   // get a reference to the current vertex.
+                // sample Simplex noise with scaling and offset transformations.
+                float noiseValue = Noise(vertex.x * scale + offsetSpeed * time, vertex.y * scale + offsetSpeed * time);
+                // Apply the value of the current vertex within the vertices.
+                vertices[i] = new Vector3(vertex.x, vertex.y, noiseValue * height + 0.3f);
+            }
+        }
+    }
+
+    private void OnDestroy() {
+        // NativeContainers must be disposed within the lifetime of the allocation. Since you’re using the persistent allocator, it’s sufficient to call Dispose() on OnDestroy().
+        waterVertices.Dispose();
+        waterNormals.Dispose();
+    }
+}
